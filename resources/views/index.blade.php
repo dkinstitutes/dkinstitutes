f8a4dab183a3fafb5ea0ee0ae467bd368b653eec
Dilawar Khan <dilawarkhan1245@gmail.com>
2022-07-23 15:20:50 +0500

    apt

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,7 +45,7 @@
                 </button>
 
                 <!-- lOGO TEXT HERE -->
-                <a href="#top" class="navbar-brand">DK INSTITUTES</a>
+                <a href="#top" class="navbar-brand">DK institutes</a>
             </div>
 
             <!-- MENU LINKS -->
