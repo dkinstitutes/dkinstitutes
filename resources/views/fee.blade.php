99a4b75e1dd61dedc8d481d9f193ee87c594077c
Dilawar Khan <dilawarkhan1245@gmail.com>
2022-08-05 10:06:11 +0500

    apt

--- resources/views/fee.blade.php
+++ resources/views/fee.blade.php
@@ -72,11 +72,11 @@
           <td>18,000</td>
         </tr>
         <tr>
-          <td>5. Search Engine Optimization</td>
+          <td>5. Search Engine Optimization(SEO)</td>
           <td>1 Months</td>
           <td>2,000</td>
-          <td>10,000</td>
-          <td>12,000</td>
+          <td>6,000</td>
+          <td>8,000</td>
         </tr>
         <tr>
           <td>6. Freelancing</td>
