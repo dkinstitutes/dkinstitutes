b6b119bdf556a33604a9f517d246b332b7e005fc
Dilawar Khan <dilawarkhan1245@gmail.com>
2022-08-29 16:01:19 +0500

    apt

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -125,7 +125,7 @@
                 <div class="col-md-4 col-sm-4">
                     <div class="feature-thumb">
                         <span>01</span>
-                        <h3>Trending Courses</h3>
+                        <h3>Our Mission</h3>
                         <p>We are providing trending and best courses in IT development, designing and E-commerce. The motive of DK INSTITUTES is to provide High-Quality Trainings with guaranteed results better for everyone. Be a part of DK INSTITUTES for better tommorow!  </p>
                     </div>
                 </div>
@@ -143,7 +143,7 @@
                 <div class="col-md-4 col-sm-4">
                     <div class="feature-thumb">
                         <span>03</span>
-                        <h3>Best IT Training Institute</h3>
+                        <h3>Pakistan's Best Institute</h3>
                         <p>DK INSTITUTES is Pakistan's Best IT Training Institute that enable students to reach their potential by providing High-Quality IT Trainings. We are the introducers to many IT
                             trends, courses, and certifications in Pakistan!</p>
                     </div>
